386763bafa487394926fe07ea38705ee8dd743c6
katsu <katsu.negishi@gmail.com>
2023-05-16 11:50:18 +0900

    データベースを更新

--- src/Entity/Kiban.php
+++ src/Entity/Kiban.php
@@ -18,11 +18,6 @@ class Kiban
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $name;
-
-    /**
      * @ORM\Column(type="string", length=255)
      */
     private $various;
@@ -42,21 +37,59 @@ class Kiban
      */
     private $stock_date;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $partsno;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $serialno;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $goodproduct;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $furikae;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nxtperson;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $assignee;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $person;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $memo;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $assigned;
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getVarious(): ?string
@@ -103,6 +136,126 @@ class Kiban
     public function setStockDate(?\DateTimeInterface $stock_date): self
     {
         $this->stock_date = $stock_date;
+
+        return $this;
+    }
+
+    public function getPartsno(): ?string
+    {
+        return $this->partsno;
+    }
+
+    public function setPartsno(string $partsno): self
+    {
+        $this->partsno = $partsno;
+
+        return $this;
+    }
+
+    public function getSerialno(): ?string
+    {
+        return $this->serialno;
+    }
+
+    public function setSerialno(?string $serialno): self
+    {
+        $this->serialno = $serialno;
+
+        return $this;
+    }
+
+    public function getGoodproduct(): ?string
+    {
+        return $this->goodproduct;
+    }
+
+    public function setGoodproduct(?string $goodproduct): self
+    {
+        $this->goodproduct = $goodproduct;
+
+        return $this;
+    }
+
+    public function getFurikae(): ?string
+    {
+        return $this->furikae;
+    }
+
+    public function setFurikae(?string $furikae): self
+    {
+        $this->furikae = $furikae;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getNxtperson(): ?string
+    {
+        return $this->nxtperson;
+    }
+
+    public function setNxtperson(?string $nxtperson): self
+    {
+        $this->nxtperson = $nxtperson;
+
+        return $this;
+    }
+
+    public function getAssignee(): ?string
+    {
+        return $this->assignee;
+    }
+
+    public function setAssignee(?string $assignee): self
+    {
+        $this->assignee = $assignee;
+
+        return $this;
+    }
+
+    public function getPerson(): ?string
+    {
+        return $this->person;
+    }
+
+    public function setPerson(?string $person): self
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    public function getMemo(): ?string
+    {
+        return $this->memo;
+    }
+
+    public function setMemo(?string $memo): self
+    {
+        $this->memo = $memo;
+
+        return $this;
+    }
+
+    public function isAssigned(): ?bool
+    {
+        return $this->assigned;
+    }
+
+    public function setAssigned(?bool $assigned): self
+    {
+        $this->assigned = $assigned;
 
         return $this;
     }
--- src/Form/KibanType.php
+++ src/Form/KibanType.php
@@ -7,16 +7,76 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+
+
 class KibanType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('various')
-            ->add('order_data')
-            ->add('due_date')
-            ->add('stock_date')
+            ->add('various', ChoiceType::class, [
+                'label' => "種別",
+                'choices' => [ 'main' => 'main', 'sub' => 'sub', 'wl' => 'wl' ],
+            ])
+            ->add('order_data', DateType::class, [
+                'label' => '発注日',
+                'required' => false,
+            ])
+            ->add('due_date', DateType::class, [
+                'label' => '納入予定日',
+                'required' => false,
+            ])
+            ->add('stock_date', DateType::class, [
+                'label' => '在庫日',
+                'required' => false,
+            ])
+            ->add('partsno', TextType::class, [
+                'label' => 'パーツ番号',
+                'required' => false,
+            ])
+            ->add('serialno', TextType::class, [
+                'label' => 'シリアル番号',
+                'required' => false,
+            ])
+            ->add('goodproduct', TextType::class, [
+                'label' => '良品・不良品',
+                'required' => false,
+            ])
+            ->add('furikae', TextType::class, [
+                'label' => '振替可否',
+                'required' => false,
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => '使用状況',
+                'choices' => [ '' => '__unselected', '未使用' => '未使用' ]
+            ])
+            ->add('nxtperson', TextType::class, [
+                'label' => 'NXT担当者',
+                'required' => false,
+            ])
+            ->add('assignee', TextType::class, [
+                'label' => '割り当て先',
+                'required' => false,
+            ])
+            ->add('person', TextType::class, [
+                'label' => '割り当て先担当者',
+                'required' => false,
+            ])
+            ->add('assigned', CheckboxType::class, [
+                'label' => '割り当て済み',
+                'required' => false,
+            ])
+            ->add('memo', TextType::class, [
+                'label' => '備考',
+                'required' => false,
+            ])
         ;
     }
 
