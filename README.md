2b2529394d8e64c437c72382aa0a4678cbd4cf07
Katsuharu Negishi <katsu.negishi@gmail.com>
2025-02-21 13:02:42 +0900

    ユーザー名のみのユーザー登録の雛形

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ symfonyは5.4 PHP7.4で作成ずみ　（PHPのバージョン変更でsymfony�
 TopControllerでメインメニューを作成済み
 必要に応じてメニュー項目を増やしてください
 
+最初のユーザーはアドミン権限がつきます
+
 # データベースを作成し、crudを作成するには？
 
 ## エンティティーを作成
@@ -15,3 +17,20 @@ bin/console make:entity エンティティー名
 ```
 bin/console make:crud 使用するエンティティー名
 ```
+
+＃このプロジェクトでは雛形に加え、以下のコマンドを実行
+
+mail address:no-replay@example.com
+
+```
+bin/console make:user
+bin/console doctrine:schema:update --dump-sql --complete
+bin/console doctrine:schema:update --dump-sql --complete --force
+bin/console make:auth
+--  Login form authenticator を選択
+bin/console make:registration-form
+bin/console make:reset-password  # usernameで作成する場合はこのバンドルが使えない
+bin/console make:crud User
+```
+
+
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Form\RegistrationFormType;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+    
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ユーザー数を数え、最初のユーザーならばADMIN権限をつける
+            $roles = $user->getRoles();
+            if ($this->userRepository->count([]) == 0) {
+                $roles[] = 'ROLE_ADMIN';
+            }
+            $user->setRoles(array_unique($roles));
+            
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
